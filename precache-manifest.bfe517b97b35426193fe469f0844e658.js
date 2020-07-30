12ef9d0339f2649277b02826d441e108d98e403e
mansourSaleh <mansouralmohsen2018@gmai.com>
2020-07-31 01:33:10 +0300

    Updates

---
+++ precache-manifest.bfe517b97b35426193fe469f0844e658.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a2e987040824cd637d141901f7f51c",
+    "url": "/maharaty/index.html"
+  },
+  {
+    "revision": "e02cb1be8132c129cefc",
+    "url": "/maharaty/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4f1089ff7435e5f2c67e",
+    "url": "/maharaty/static/css/main.2aedbee8.chunk.css"
+  },
+  {
+    "revision": "e02cb1be8132c129cefc",
+    "url": "/maharaty/static/js/2.2f33eec9.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/maharaty/static/js/2.2f33eec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1089ff7435e5f2c67e",
+    "url": "/maharaty/static/js/main.5e392ed6.chunk.js"
+  },
+  {
+    "revision": "15761a438ed8946d0b9a",
+    "url": "/maharaty/static/js/runtime-main.da2977fb.js"
+  },
+  {
+    "revision": "dd99bd80f1b7fb397a946c9d76a5cc1e",
+    "url": "/maharaty/static/media/bg3.dd99bd80.jpg"
+  }
+]);
