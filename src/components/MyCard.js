f7df771b88f500a28e8423984af9048a4fd1456b
mansourSaleh <mansouralmohsen2018@gmai.com>
2020-08-11 18:01:17 +0300

    add

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -14,7 +14,7 @@ function MyCard({icon, title, description, buttonTitle}) {
   }}
     transition={{ duration: 0.4 }}
     whileHover={{ scale: 1.1, opacity: 1 }}
-    whileTap={{ scale: 0.8 }}
+    // whileTap={{ scale: 0.8 }}
     className="mt-5 ml-5 mr-5 pt-3 text-right card-choose d-flex flex-column" style={{ width: "18rem",
      borderColor: "#02c39A", borderWidth: "0.5px", borderStyle: 'solid' 
     }}>
@@ -24,7 +24,12 @@ function MyCard({icon, title, description, buttonTitle}) {
         <Card.Text className="font-des">
           {description}
         </Card.Text>
-        <Button  className="d-flex mr-auto ml-auto pl-5 pr-5 font-des" style={{backgroundColor: "#028090" , borderColor: "#028090"}}>{buttonTitle}</Button>
+        
+        <a href="http://nutritious-eggs.surge.sh">
+        <Button   className="d-flex mr-auto ml-auto pl-5 pr-5 font-des" style={{backgroundColor: "#028090" , borderColor: "#028090"}}>
+        {buttonTitle}
+        </Button>
+        </a>
       </Card.Body>
     </motion.Card>
   );
