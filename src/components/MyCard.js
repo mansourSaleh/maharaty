bf51f608199b2acb00b66ece23855658f1b268ab
mansourSaleh <mansouralmohsen2018@gmai.com>
2020-09-11 11:42:34 +0300

    added one page

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -27,7 +27,7 @@ function MyCard({icon, title, description, buttonTitle}) {
         </Card.Text>
         
         
-        <a href="https://mansoursaleh.github.io/maharaty2/">
+        <a href="https://main.xn--mgbaj0a2hmz.com/">
         <Button   className="d-flex mr-auto ml-auto pl-5 pr-5 font-des" style={{backgroundColor: "#28BECD" , borderColor: "#28BECD"}}>
        {/* <Link to="" > */}
        {buttonTitle}
